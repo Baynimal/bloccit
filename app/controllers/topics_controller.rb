4795f42803372ec914d82e3ebb480d11e6a28ade
Andrew Baynes <dru_baynes@hotmail.com>
2016-08-30 17:51:48 -0700

    adding authorization for user roles for bloccit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,7 @@
 class TopicsController < ApplicationController
 
+   
+
   def index
     @topics = Topic.all
   end
@@ -54,8 +56,17 @@ class TopicsController < ApplicationController
     end
   end
 
+
+  private
   def topic_params
     params.require(:topic).permit(:name, :description, :public)
-  end 
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,9 +35,16 @@ post = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'dru_baynes@hotmail.com',
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
   password: 'helloworld'
 )
 
