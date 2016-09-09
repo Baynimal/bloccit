f2dc1fcaddc0f187a4abb90a294f813fb2bbb229
Andrew Baynes <dru_baynes@hotmail.com>
2016-09-08 18:12:56 -0700

    debugged seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,12 @@ topics = Topic.all
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
-end
-
   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
+end
+post = Post.all
+
 
 100.times do
   Comment.create!(
