d708cfbb450854ee91f84badc55249e5d4657889
Andrew Baynes <dru_baynes@hotmail.com>
2016-09-12 15:35:54 -0700

    add ajax to votes resource

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
@@ -92,12 +93,12 @@ end
     end
 
     it "returns 'nil' if the user has not favorited the post" do
-      expect(user.favorite_for(@post)).to be_nil
+      expect(other_user.favorite_for(@post)).to be_nil
     end
 
     it "returns the appropriate favorite if it exists" do
-      favorite = user.favorites.where(post: @post).create
-      expect(user.favorite_for(@post)).to eq(favorite)
+      favorite = other_user.favorites.where(post: @post).create
+      expect(other_user.favorite_for(@post)).to eq(favorite)
     end
   end
 
